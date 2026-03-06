7afca1e4496e1c0d465bfc91c7c8cef06dfe4c79
Julio Gaiotto <6553765+juliogaiotto@users.noreply.github.com>
2026-03-06 10:07:14 -0300

    Adicionado aula Dia3

---
+++ Aulas/Dia3/Program.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+Console.WriteLine("\nGenerating invoices for customer \"Contoso Corp\" ... \n");
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.Write("\nOutput Directory:\t");
